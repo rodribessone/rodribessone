5c7297361fb7bac4b205c766779ce5b78b576c52
rodribessone <rodrigo-bessone@hotmail.com>
2025-02-20 19:44:47 +1000

    cambio imagen fondo

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons"
