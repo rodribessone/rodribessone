2577aa9befbcc7821da5732b52da7012b864aa84
rodribessone <rodrigo-bessone@hotmail.com>
2025-02-23 01:40:13 +1000

    google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,18 @@ import Proyectos from './components/Proyectos/Proyectos'
 import Skills from './components/Skills/Skills'
 import Contacto from './components/Contacto/Contacto'
 import Header from "./components/Header/Header"
+import ReactGA from "react-ga4"
+import { useEffect } from 'react'
+
+ReactGA.initialize("G-04YSTZ9G0M")
+
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.send("pageview");
+  }, []);
+
   return (
     <div className='w-full flex flex-col relative '>
       <Header />
